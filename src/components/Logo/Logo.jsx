f6007aee0d63286197d6cc658b4a610d3fdfc968
PopovVK <vladsmartvladsmart@gmail.com>
2023-09-04 17:16:22 +0300

    Updates

--- src/components/Logo/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import logoImage from './logo.png'; 
-import './Logo.css';
-
-const Logo = () => {
-  return <img className="logo-image" src={logoImage} alt="Logo" />;
-};
-
-export default Logo;
